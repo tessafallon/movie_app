dfcff7406546aa59a50d2f7b38c756d86f72e252
MsUzoAgu <uzolearns@gmail.com>
2014-01-14 19:33:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieApp::Application.config.secret_token = 'fd11c98ee477cad8087df54ddd59f8b8dd55c91d8962a032bf3740e26e479cc43eaa6f4495f71c8ea8d877f431f0d42bea94ae769e69d8df8bd43e79d0cd6a13'
